6e012f5a57ff5a02d8256ee619a1b4795299c49c
项志远 <2738186032@qq.com>
2023-02-16 20:11:07 +0800

    change the write way

--- convert.py
+++ convert.py
@@ -1,11 +1,12 @@
 import os
 from multiprocessing import Process
 import multiprocessing
-from datasketch import MinHash
+# from datasketch import MinHash
 from utils import str_encode, max_size, get_all_files, number_to_string
 import numpy as np
 import jsonlines
 import hashlib
+# from charset_mnbvc import api
 
 
 # # 计算该行的minhash值
@@ -20,7 +21,7 @@ import hashlib
 # 计算一个长为32的minhash值
 def from_txt_to_json(file_path, threshold, num_perm=32):
     hashs = set()
-    f = open(file_path, 'r')
+    f = open(file_path, 'r', encoding='utf-8', errors='ignore')
     file_json = {'file_name': file_path,
                  'simhash':'',
                  'suspected': False,
@@ -34,7 +35,7 @@ def from_txt_to_json(file_path, threshold, num_perm=32):
         line = line.strip()
         if len(line) == 0:
             continue
-        h = hashlib.md5(line).hexdigest()
+        h = hashlib.md5(line.encode()).hexdigest()
         hashs.add(h)
         file_json['content'].append({'line_number': len(file_json['content']) + 1,
                                      'line_content': line,
@@ -53,41 +54,51 @@ def from_txt_to_json(file_path, threshold, num_perm=32):
     return file_json
 
 
-def run_process(file_path_queue, count, lock, dst_dir, threshold):
-    lock.acquire()
-    last_file_path = os.path.join(os.getcwd(), dst_dir, str(count.value) +'.jsonl')
-    count.value += 1
-    lock.release()
-
-    file_to_write = jsonlines.open(last_file_path, mode='w')
-
-    count = 0
+def run_process(file_path_queue, json_to_write_queue, threshold):
 
     while not file_path_queue.empty():
-        count += 1
         file_path = file_path_queue.get()
-
         one_json = from_txt_to_json(file_path, threshold)
-
-        file_to_write.write(one_json)
-        
-        if os.path.getsize(last_file_path) >= max_size:
-            file_to_write.close()
-            print('File {} is full.'.format(last_file_path))
-            print('Now there are {} files.'.format(count))
-            count = 0
-            lock.acquire()
-            last_file_path = os.path.join(os.getcwd(), dst_dir, str(count.value) +'.jsonl')
-            count.value += 1
-            lock.release()
-            file_to_write = jsonlines.open(last_file_path, mode='w')
-
-    file_to_write.close()
+        json_to_write_queue.put(one_json)
 
 
+def write_jsonl(json_to_write_queue, processes, dst_dir):
+    count = 0
+    last_file = jsonlines.open(os.path.join(dst_dir, str(count) + '.jsonl'), mode='w')
+    problem_count = 0
+    last_problem_file = jsonlines.open(os.path.join(dst_dir, 'problem_'+str(problem_count) + '.jsonl'), mode='w')
+    
+    while True:
+        if not json_to_write_queue.empty():
+            one_json = json_to_write_queue.get()
+            if one_json['suspected']:
+                last_problem_file.write(one_json)
+                if last_problem_file._fp.tell() > max_size:
+                    last_problem_file.close()
+                    problem_count += 1
+                    last_problem_file = jsonlines.open(os.path.join(dst_dir, 'problem_'+str(problem_count) + '.jsonl'), mode='w')
+            else:
+                last_file.write(one_json)
+                if last_file._fp.tell() > max_size:
+                    last_file.close()
+                    count += 1
+                    last_file = jsonlines.open(os.path.join(dst_dir, str(count) + '.jsonl'), mode='w')
+        else:96
+            has_alive = False
+            for p in processes:
+                if p.is_alive():
+                    has_alive = True
+                    break
+            if not has_alive:
+                return
+            
 
 
-def convert(src_dir, src='txt', dst='jsonl', dst_dir='converted/', n_process = 4, threshold = 0.99):
+
+
+
+
+def convert(src_dir, src='txt', dst='jsonl', dst_dir='converted/', n_process = 4, threshold = 0.95):
     assert os.path.exists(src_dir)
     assert src in ['txt', 'jsonl']
     assert dst in ['txt', 'jsonl']
@@ -98,16 +109,16 @@ def convert(src_dir, src='txt', dst='jsonl', dst_dir='converted/', n_process = 4
         raise NotImplementedError('Only support converting from txt to jsonl now.')
 
     file_path_queue = get_all_files(src_dir)
-
-    lock = multiprocessing.Lock()
-    count = multiprocessing.Value('i', 0)
+    json_to_write_queue = multiprocessing.Queue(200)
 
     processes = []
 
     for _ in range(n_process):
-        p = Process(target = run_process, args = (file_path_queue, count, lock, dst_dir, threshold))
+        p = Process(target = run_process, args = (file_path_queue, json_to_write_queue, threshold))
         p.start()
         processes.append(p)
+    
+    write_jsonl(json_to_write_queue, processes, dst_dir)
     for p in processes:
         p.join()
 
--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import multiprocessing
 
 
 if __name__ == '__main__':
-    convert('/home/xiang/文档/20230124', dst_dir='converted/', n_process=multiprocessing.cpu_count()-1)
-    remove_duplicates('converted/', n_process=multiprocessing.cpu_count()-1, windows_size=5)
+    convert('/home/xiang/文档/20230124', dst_dir='./converted/', n_process=multiprocessing.cpu_count()-2, threshold=0.9)
+    # remove_duplicates('converted/', n_process=multiprocessing.cpu_count()-1, windows_size=5)
