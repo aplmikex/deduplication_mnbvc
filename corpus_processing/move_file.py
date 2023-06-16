bcc680cfbd1c620f0822d927bc3f3a0be2c7111a
项志远 <2738186032@qq.com>
2023-06-16 19:29:09 +0800

    添加根据关键词move以及后缀名大小写都匹配的代码

--- corpus_processing/move_file.py
+++ corpus_processing/move_file.py
@@ -2,11 +2,13 @@ import os
 import shutil
 import argparse
 
-def move_files(input_dir, output_dir, suffix):
+def move_files(input_dir, output_dir, suffix, keywords:str):
     if os.path.exists(input_dir) == False:
         raise ValueError('输入目录不存在')
     if os.path.abspath(input_dir) == os.path.abspath(output_dir):
         raise ValueError('输入目录和输出目录不能相同')
+
+    keywords = [keyword for keyword in keywords.split(',') if keyword != '']
 
     os.makedirs(output_dir, exist_ok=True)
     for root, _, files in os.walk(input_dir):
@@ -18,7 +20,14 @@ def move_files(input_dir, output_dir, suffix):
         first_create = True
         # 移动符合条件的文件
         for file in files:
-            if file.endswith(suffix):
+            file_path = os.path.join(root, file)
+            # 检查file_path是否包含全部关键词
+            in_keywords = True
+            for keyword in keywords:
+                if keyword not in file_path:
+                    in_keywords = False
+                    break
+            if file.lower().endswith(suffix) and in_keywords:
                 if first_create:
                     os.makedirs(target_dir, exist_ok=True)
                     first_create = False
@@ -32,6 +41,7 @@ if __name__ == "__main__":
     parser.add_argument('--input_dir', type=str, required=True, help="输入目录")
     parser.add_argument('--output_dir', type=str, required=True, help="输出目录")
     parser.add_argument('--suffix', type=str, required=True, help="后缀名")
+    parser.add_argument('--keywords', type=str, default='', help="关键词")
 
     args = parser.parse_args()
-    move_files(args.input_dir, args.output_dir, args.suffix)
+    move_files(args.input_dir, args.output_dir, args.suffix, args.keywords)
